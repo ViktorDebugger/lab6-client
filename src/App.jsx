683fc92af92e9817754512159464c38d5af96115
ViktorDebugger <lv418153@gmail.com>
2025-05-08 10:07:57 +0300

    Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
+import "./index.css";
 import Header from "./components/Header.jsx";
 import Footer from "./components/Footer.jsx";
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home.jsx";
 import Publications from "./pages/Publications.jsx";
 import Profile from "./pages/Profile.jsx";
 import Login from "./pages/Login.jsx";
 import Register from "./pages/Register.jsx";
+import Likes from "./pages/Likes.jsx";
 
 function App() {
-
-
   return (
     <Router>
       <div className="relative bg-cover bg-center bg-no-repeat bg-custom-bg min-h-screen flex flex-col text-white">
@@ -19,6 +19,7 @@ function App() {
             <Route path="/" element={<Home />} />
             <Route path="/profile" element={<Profile />} />
             <Route path="/publications" element={<Publications />} />
+            <Route path="/likes" element={<Likes />} />
             <Route path="/login" element={<Login />} />
             <Route path="/register" element={<Register />} />
           </Routes>
